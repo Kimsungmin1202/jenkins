95b7271f951aab2f6e093f9a69979f9059660a64
Kimsungmin1202 <dddaaa1202@naver.com>
2025-08-14 15:20:05 +0900

    Update providers.tf

--- live/stage/network/providers.tf
+++ live/stage/network/providers.tf
@@ -1,5 +1,10 @@
 provider "aws" {
   region = var.region
+  assume_role {
+    role_arn     = "arn:aws:iam::095808160461:role/TerraformDeployRole"
+    session_name = "jenkins-terraform"
+    # external_id = "cjenm-deploy"   # 3단계에서 External ID 사용했다면 이 줄 주석 해제
+  }
 }
 data "aws_availability_zones" "available" {
   state = "available"
