858f855cd93054f000a849d4777a4d5bed232928
Kimsungmin1202 <dddaaa1202@naver.com>
2025-08-14 10:04:23 +0900

    Update variables.tf

--- live/stage/network/variables.tf
+++ live/stage/network/variables.tf
@@ -1,6 +1,28 @@
-variable "region"   { type = string  default = "ap-northeast-2" }
-variable "vpc_name" { type = string }
-variable "vpc_cidr" { type = string }
-variable "azs"      { type = list(string)  default = null } # null → first 2 AZs
-variable "nat_mode" { type = string default = "single" }    # single | per_az | none
-variable "tags"     { type = map(string) default = { ManagedBy = "Jenkins", Project = "vpc" } }
+variable "region" {
+  type    = string
+  default = "ap-northeast-2"
+}
+
+variable "vpc_name" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+# null이면 첫 2개 AZ 자동 사용
+variable "azs" {
+  type    = list(string)
+  default = null
+}
+
+variable "nat_mode" {
+  type    = string
+  default = "single"  # single | per_az | none
+}
+
+variable "tags" {
+  type    = map(string)
+  default = { ManagedBy = "Jenkins", Project = "vpc" }
+}
