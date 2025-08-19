2c608b77cef2a7c7d0b3c37c6ed8f58abb6a11a3
Kimsungmin1202 <dddaaa1202@naver.com>
2025-08-19 11:29:25 +0900

    Update main.tf

--- live/dev/network/main.tf
+++ live/dev/network/main.tf
@@ -5,7 +5,12 @@ module "network" {
   azs      = var.azs == null ? slice(data.aws_availability_zones.available.names, 0, 2) : var.azs
   nat_mode = var.nat_mode
   tags     = var.tags
+
+  # 추가
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
 }
+
 output "vpc_id"             { value = module.network.vpc_id }
 output "public_subnet_ids"  { value = module.network.public_subnet_ids }
 output "private_subnet_ids" { value = module.network.private_subnet_ids }
